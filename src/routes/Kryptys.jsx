f7891ad498231b8f38de173c262ba32fbcd4170e
“Kamile.Paukste” <kamile.paukste1@gmail.com>
2024-06-20 15:24:32 +0300

    Hero section added to all pages

--- src/routes/Kryptys.jsx
+++ src/routes/Kryptys.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
+import Heroimg from "../components/heroimg";
 
 const Kryptys = () => {
   return (
     <div>
       <Navbar />
+      <Heroimg
+        heading="Directions"
+        text="Looking for an exotic trip? Choose any destination you like; we offer trips to Bali, Madagascar, the Maldives, and the Seychelles."
+      />
       <Footer />
     </div>
   );
--- src/routes/Susisiekite.jsx
+++ src/routes/Susisiekite.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
+import Heroimg from "../components/heroimg";
 
 const Susisiekite = () => {
   return (
     <div>
       <Navbar />
+      <Heroimg
+        heading="Contact Us!"
+        text="Have questions or need assistance planning your trip? Reach out to our friendly customer service team via phone, email, or our online contact form."
+      />
       <Footer />
     </div>
   );
