b5de89db7d2c2eb867b28b2394996c40bca9d168
“Kamile.Paukste” <kamile.paukste1@gmail.com>
2024-06-21 16:36:13 +0300

    Backend failed. Directions changed from html to js

--- src/components/directions.jsx
+++ src/components/directions.jsx
@@ -1,52 +1,30 @@
 import "./directions.css";
 import React from "react";
 import maldives from "../assets/maldives.jpg";
+import { Link } from "react-router-dom";
+
+const DirectionCard = ({ name, imgSrc, link }) => (
+  <div className="directionsCard">
+    <h2 className="directionName">{name}</h2>
+    <img src={imgSrc} alt={`${name} island`} />
+    <div>
+      <p>Details</p>
+      <Link to={link}>
+        <button>Price</button>
+      </Link>
+    </div>
+  </div>
+);
 
 const Directions = () => {
   return (
     <div className="directions">
       <h1>Choose your next trip</h1>
       <div className="directionsContainer">
-        <div className="directionsCard">
-          <h2 className="directionName">Maldives</h2>
-          <img src={maldives} alt="maldives island" />
-          <div>
-            <p>Details</p>
-            <button Link to="/Kainos">
-              Price
-            </button>
-          </div>
-        </div>
-        <div className="directionsCard">
-          <h2 className="directionName">Maldives</h2>
-          <img src={maldives} alt="maldives island" />
-          <div>
-            <p>Details</p>
-            <button Link to="/Kainos">
-              Price
-            </button>
-          </div>
-        </div>
-        <div className="directionsCard">
-          <h2 className="directionName">Maldives</h2>
-          <img src={maldives} alt="maldives island" />
-          <div>
-            <p>Details</p>
-            <button Link to="/Kainos">
-              Price
-            </button>
-          </div>
-        </div>
-        <div className="directionsCard">
-          <h2 className="directionName">Maldives</h2>
-          <img src={maldives} alt="maldives island" />
-          <div>
-            <p>Details</p>
-            <button Link to="/Kainos">
-              Price
-            </button>
-          </div>
-        </div>
+        <DirectionCard name="Maldives" imgSrc={maldives} link="/Kainos" />
+        <DirectionCard name="Bali" imgSrc={maldives} link="/Kainos" />
+        <DirectionCard name="Madagascar" imgSrc={maldives} link="/Kainos" />
+        <DirectionCard name="Seychelles" imgSrc={maldives} link="/Kainos" />
       </div>
     </div>
   );
