5b57d42f40932109903a1e5c5cf96d0f9fd3e634
“Kamile.Paukste” <kamile.paukste1@gmail.com>
2024-06-21 17:46:53 +0300

    Directions cards

--- src/components/directions.jsx
+++ src/components/directions.jsx
@@ -1,14 +1,32 @@
 import "./directions.css";
 import React from "react";
 import maldives from "../assets/maldives.jpg";
+import bali from "../assets/balidirections.jpg";
+import madagascar from "../assets/madagascar.jpg";
+import seychelles from "../assets/seychelles.jpg";
 import { Link } from "react-router-dom";
 
-const DirectionCard = ({ name, imgSrc, link }) => (
+const DirectionCard = ({
+  name,
+  imgSrc,
+  link,
+  nights,
+  hotelName,
+  allInclusive,
+}) => (
   <div className="directionsCard">
     <h2 className="directionName">{name}</h2>
     <img src={imgSrc} alt={`${name} island`} />
-    <div>
-      <p>Details</p>
+    <div className="details">
+      <div>
+        <p>Number of Nights: {nights}</p>
+      </div>
+      <div>
+        <p>Hotel: {hotelName}</p>
+      </div>
+      <div>
+        <p>All Inclusive: {allInclusive ? "Yes" : "No"}</p>
+      </div>
       <Link to={link}>
         <button>Price</button>
       </Link>
@@ -21,10 +39,38 @@ const Directions = () => {
     <div className="directions">
       <h1>Choose your next trip</h1>
       <div className="directionsContainer">
-        <DirectionCard name="Maldives" imgSrc={maldives} link="/Kainos" />
-        <DirectionCard name="Bali" imgSrc={maldives} link="/Kainos" />
-        <DirectionCard name="Madagascar" imgSrc={maldives} link="/Kainos" />
-        <DirectionCard name="Seychelles" imgSrc={maldives} link="/Kainos" />
+        <DirectionCard
+          name="Maldives"
+          imgSrc={maldives}
+          link="/Kainos"
+          nights={7}
+          hotelName="Sunrise Beach Hotel"
+          allInclusive={true}
+        />
+        <DirectionCard
+          name="Bali"
+          imgSrc={bali}
+          link="/Kainos"
+          nights={5}
+          hotelName="Ocean View Resort"
+          allInclusive={false}
+        />
+        <DirectionCard
+          name="Madagascar"
+          imgSrc={madagascar}
+          link="/Kainos"
+          nights={10}
+          hotelName="Rainforest Retreat"
+          allInclusive={true}
+        />
+        <DirectionCard
+          name="Seychelles"
+          imgSrc={seychelles}
+          link="/Kainos"
+          nights={8}
+          hotelName="Paradise Cove"
+          allInclusive={false}
+        />
       </div>
     </div>
   );
