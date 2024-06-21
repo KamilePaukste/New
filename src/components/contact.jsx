39a6ea6424b384197e4f81c70c23694c6b906735
“Kamile.Paukste” <kamile.paukste1@gmail.com>
2024-06-21 19:20:36 +0300

    Contact form

---
+++ src/components/contact.jsx
@@ -0,0 +1,24 @@
+import "./contact.css";
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="form">
+      <form>
+        <label>Your name</label>
+        <input type="text"></input>
+        <label>Email</label>
+        <input type="email"></input>
+        <label>Phone number</label>
+        <input type="number"></input>
+        <label>Your message</label>
+        <textarea rows="5" placeholder="Type Your Message Here">
+          {" "}
+        </textarea>
+        <button className="button">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
--- src/routes/Susisiekite.jsx
+++ src/routes/Susisiekite.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
 import Heroimg from "../components/heroimg";
+import Contact from "../components/contact";
 
 const Susisiekite = () => {
   return (
@@ -11,6 +12,7 @@ const Susisiekite = () => {
         heading="Contact Us!"
         text="Have questions or need assistance planning your trip? Reach out to our friendly customer service team via phone, email, or our online contact form."
       />
+      <Contact />
       <Footer />
     </div>
   );
